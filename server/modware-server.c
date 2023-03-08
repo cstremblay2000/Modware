385ba8b2f975795b1f82fc613183380ba2ef0bcb
cst <cstremblay2000@gmail.com>
2023-03-07 22:05:24 -0500

    init commit

---
+++ server/modware-server.c
@@ -0,0 +1,16 @@
+/**
+ * file:        modware-server.c
+ * author:      Chris Tremblay <cst1465@rit.edu>
+ * language:    C
+ * date:        03/07/2023, Happy National Cereal Day!
+ * description:
+ *  TODO
+ */
+
+#include <gcrypt.h>
+
+int main( int argc, char **argv )
+{
+
+    return 0;
+}
