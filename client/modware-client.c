8b692fbd50020b1dd872effd4fbd2cbd0d382193
cst <cstremblay2000@gmail.com>
2023-03-07 22:30:16 -0500

    init commit

--- client/modware-client.c
+++ client/modware-client.c
@@ -1,14 +1,21 @@
 /**
  * file:        modware-client.c
  * author:      Chris Tremblay <cst1465@rit.edu>
- * language:    C
  * date:        03/07/2023, Happy National Cereal Day!
+ * language:    C
  * description:
  *  TODO
  */
 
 #include <gcrypt.h>
 
+/** 
+ * description:
+ *  The driver functoin
+ * parameters:
+ *  argc -> arg count
+ *  argv -> args
+ */
 int main( int argc, char **argv )
 {
 
