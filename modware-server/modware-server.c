6031631579dfde7c9edd14fd0d217c0690b52ed1
cst <cstremblay2000@gmail.com>
2023-03-21 19:12:34 -0400

    rename again

---
+++ modware-server/modware-server.c
@@ -0,0 +1,23 @@
+/**
+ * file:        modware-server.c
+ * author:      Chris Tremblay <cst1465@rit.edu>
+ * date:        03/07/2023, Happy National Cereal Day!
+ * language:    C
+ * description:
+ *  TODO
+ */
+
+#include <gcrypt.h>
+
+/** 
+ * description:
+ *  The driver functoin
+ * parameters:
+ *  argc -> arg count
+ *  argv -> args
+ */
+int main( int argc, char **argv )
+{
+
+    return 0;
+}
